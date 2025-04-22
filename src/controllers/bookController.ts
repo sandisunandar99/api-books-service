affd414405e45675d460a15bac57f781da04833d
Sandi <sandisunandar99@gmail.com>
2025-04-23 06:20:24 +0700

    [chore] : change validation default to joi validation

--- src/controllers/bookController.ts
+++ src/controllers/bookController.ts
@@ -1,19 +1,26 @@
 import { Request, Response } from "express";
-import { validationResult } from "express-validator";
-import { BookInput, bookModel } from "../models/Book.js";
+import { BookInput, bookModel } from "../models/Book";
+import { bookSchema } from "../validations/bookValidation";
 export const createBook = async (req: Request, res: Response) => {
   try {
     
     //validation param before insert
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
+    const { error, value } = bookSchema.validate(req.body, {
+      abortEarly: false,
+    });
+
+    if (error) {
+      return res.status(400).json({
+        errors: error.details.map((detail) => ({
+          field: detail.context?.key,
+          message: detail.message,
+        })),
+      });
     }
 
     //define model for input data
     const bookInput: BookInput = req.body;
     const newBook = await bookModel.create(bookInput);
-
 
     return res.status(201).json(newBook);
   } catch (error) {
