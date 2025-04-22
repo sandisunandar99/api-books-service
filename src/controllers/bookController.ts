1b3d1c57b2afa54cad836c524528d64499537428
Sandi <sandisunandar99@gmail.com>
2025-04-23 06:03:56 +0700

    [feat] : define create data book

--- src/controllers/bookController.ts
+++ src/controllers/bookController.ts
@@ -1,10 +1,21 @@
 import { Request, Response } from "express";
-
+import { validationResult } from "express-validator";
+import { BookInput, bookModel } from "../models/Book.js";
 export const createBook = async (req: Request, res: Response) => {
   try {
     
+    //validation param before insert
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
 
-    return res.status(201).json({"message": "Book created successfully" });
+    //define model for input data
+    const bookInput: BookInput = req.body;
+    const newBook = await bookModel.create(bookInput);
+
+
+    return res.status(201).json(newBook);
   } catch (error) {
     console.error("Error creating book:", error);
     return res.status(500).json({ message: "Internal server error" });
--- src/models/Book.ts
+++ src/models/Book.ts
@@ -1,3 +1,8 @@
+import { v4 as uuidv4 } from "uuid";
+import { AppDataSource } from "../database/data-source.js";
+import { Book } from "../entities/Book.js";
+import { Genre } from "../entities/Genre.js";
+
 export interface BookInput {
   title: string;
   author: string;
@@ -6,9 +11,43 @@ export interface BookInput {
   stock: number;
 }
 
-
 export class BookModel {
+  private bookRepository = AppDataSource.getRepository(Book);
+  private genreRepository = AppDataSource.getRepository(Genre);
 
+  async create(bookInput: BookInput): Promise<Book> {
+    const book = new Book();
+    book.title = bookInput.title;
+    book.author = bookInput.author;
+    book.publishedYear = bookInput.publishedYear;
+    book.stock = bookInput.stock;
+
+    // Handle genres
+    book.genres = await this.getOrCreateGenres(bookInput.genres);
+
+    return this.bookRepository.save(book);
+  }
+
+  private async getOrCreateGenres(genreNames: string[]): Promise<Genre[]> {
+    const genres: Genre[] = [];
+
+    for (const name of genreNames) {
+      let genre = await this.genreRepository.findOne({
+        where: { name },
+      });
+
+      // if genre not add or exist, create it
+      if (!genre) {
+        genre = new Genre();
+        genre.name = name;
+        await this.genreRepository.save(genre);
+      }
+
+      genres.push(genre);
+    }
+
+    return genres;
+  }
 }
 
 // Singleton instance for the application
