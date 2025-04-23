54909ad41bc432ed49c32de81905a6ce9ad7b5d3
Sandi <sandisunandar99@gmail.com>
2025-04-23 21:12:44 +0700

    [info] : swagger

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,9 @@ const PORT = process.env.PORT || 3000;
 
 const server = app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
+  
+  // swagger url 
+  console.log(`Swagger UI: http://localhost:${PORT}/api-docs`);
 });
 
 
